27ca4f04d6b0a6a59372ad7364384c0e12f65953
leandrosardi <leandro.sardi@expandedventure.com>
2024-01-31 11:50:11 -0300

    coding

--- test.js
+++ test.js
@@ -249,10 +249,150 @@ var facebook_group_posts = {
 };
 
 // ----------------------------
+var facebook_public_feed = {
+    // browser must be located at this URL: https://www.facebook.com/?filter=groups&sk=h_chr
+    // perform an AJAX request to reload the list of posts.
+    load: function () {
+        // TODO: Code Me!
+    },
+
+    // process a response to a XHR request.
+    // if it is about a post, parse it and add it to the data array.
+    scrape: function (xhr) {
+        // show the request url by default
+        //console.log('- URL: ' + xhr._url);
+        var s = null; // complete response text 
+        var x = null; // line response text
+        var t = null; // response text wrapped in array
+        var j = null; // response json
+        var o = null; // response json -> data object
+        var ar = null;
+        var obj = {}; // response json in massprospecting format 
+
+        //console.log("Ready URL: " + xhr._url);
+
+        if (xhr._url == '/api/graphql/') {
+            s = xhr.responseText;
+            // facebook uses to return many json objects in one response, so we need to split the response by newline
+            ar = s.split("\n");
+            // iterate array ar
+            for (let z = 0; z < ar.length; z++) {
+                x = ar[z];
+                // set obj to empty object
+                obj = {};
+                // JSON is not a valid json, you must wrap it in array.
+                // reference: https://stackoverflow.com/questions/51172387/json-parse-unexpected-non-whitespace-character-after-json-data-at-line-1-column
+                t = '['+x+']';
+                j = JSON.parse(t)[0];
+                
+                if (x.startsWith('{"label":"CometNewsFeed_viewerConnection$stream$CometNewsFeed_viewer_news_feed"')) {
+                    o = j.data
+                }
+                
+                if (x.startsWith('{"data":{"viewer":{"news_feed":')) {
+                    // post
+                    o = j.data.viewer.news_feed.edges[0]; 
+                }
+    
+                if (o != null) {
+                    // add the raw json descriptor to the object
+                    // for further analysis and debugging.
+                    obj['raw'] = o;
+
+                    //console.log('------------------');
+
+                    // # of comments
+                    obj['comments'] = o.node.comet_sections.feedback.story.feedback_context.feedback_target_with_context.ufi_renderer.feedback.total_comment_count
+
+                    // # of reactions
+                    obj['reactions'] = o.node.comet_sections.feedback.story.feedback_context.feedback_target_with_context.ufi_renderer.feedback.comet_ufi_summary_and_actions_renderer.feedback.i18n_reaction_count
+
+                    // # of shares
+                    obj['shares'] = o.node.comet_sections.feedback.story.feedback_context.feedback_target_with_context.ufi_renderer.feedback.comet_ufi_summary_and_actions_renderer.feedback.i18n_share_count
+
+                    // post content, 
+                    let a = o.node.comet_sections.content.story.message;
+                    if (a == null) {
+                        obj['body'] = null;
+                    } else {
+                        obj['body'] = a.text;
+                        //console.log('BODY: ' + obj['body']);
+                    }
+
+                    // URL of all photos in the post,
+                    let b = o.node.comet_sections.content.story.attachments; 
+                    obj['images'] = [];
+                    // iterate array b
+                    for (let i = 0; i < b.length; i++) {
+                        // if it is an image, add it to the images array
+                        let attachment = b[i].styles.attachment;
+                        if (attachment != null && attachment != undefined) {
+                            let media = attachment.media;
+                            if (media != null && media != undefined) {
+                                let img = media.photo_image;
+                                if (img != null && img != undefined) {
+                                    obj['images'].push( img.uri );
+                                }                        
+                            }
+                            // if the is a list of sub-attachments, iterate it
+                            let subattachments = attachment.all_subattachments;
+                            if (subattachments != null && subattachments != undefined) {
+                                for (let i2 = 0; i2 < subattachments.nodes.length; i2++) {
+                                    let img = subattachments.nodes[i2].media.image;
+                                    if (img != null && img != undefined) {
+                                        obj['images'].push( img.uri );
+                                    }                        
+                                }
+                            }
+                        } // end if attachment
+                    }
+
+                    // direct id or link to the post, 
+                    let c = o.node.post_id;
+                    obj['post_id'] = c;
+
+                    // direct id or link to the Facebook group where such content has been posted,
+                    // name of the facebook groups
+                    let d = o.node.comet_sections.context_layout.story.comet_sections.actor_photo.story.to;
+                    obj['group'] = {}
+                    obj['group']['id'] = d.id;
+                    obj['group']['name'] = d.name;
+                    
+                    //console.log('GROUP: ' + obj['group']['name']);
+                    
+                    // name of the Facebook user who posted,
+                    // link to the Facebook profile of such a user,
+                    // URL of the picture of such a Facebook user.
+                    //let e = o.node.comet_sections.content.story.actors[0];
+                    let e = o.node.comet_sections.context_layout.story.comet_sections.actor_photo.story.actors[0];
+                    obj['lead'] = {}
+                    obj['lead']['id'] = e.id;
+                    obj['lead']['name'] = e.name;
+                    obj['lead']['url'] = e.url;
+                    obj['lead']['profile_picture'] = e.profile_picture.uri
+
+                    // if not exists and object into $$.data with the same post_id,
+                    // add this result to the data array
+                    let exists = false;
+                    for (let i = 0; i < $$.data.length; i++) {
+                        if ($$.data[i]['post_id'] == obj['post_id']) {
+                            exists = true;
+                        }
+                    }
+                    if (!exists) {
+                        $$.push(obj);
+                    }
+                }
+            }
+        }
+    },
+};
+
+// ----------------------------
 
 $$.init({
     parse: function(xhr) {
-        facebook_group_posts.scrape(xhr);
+        facebook_public_feed.scrape(xhr);
     }
 });
 
