0922d03f9e31ab70ec9a8afe88ab513a1e684c6f
leandrosardi <leandro.sardi@expandedventure.com>
2024-02-02 02:50:39 -0300

    docu

--- examples/selenium.rb
+++ examples/selenium.rb
@@ -13,6 +13,16 @@ id = 'jdus77h'
 # Starting and Operating browser
 driver = client.driver(id)
 
+# Get source code of inject.js library using OpenURI library
+uri = URI.parse('https://raw.githubusercontent.com/leandrosardi/intercept/main/lib/intercept.js')
+js1 = Net::HTTP.get(uri)
+
+# Get the source code of the scraper
+js2 = File.read('./facebook_group_posts.js')
+
+# Execute the scraper
+# Inject the library into the page
+#
 # Selenium: How to Inject/execute a Javascript in to a Page before loading/executing any other scripts of the page?
 #
 # Reference:
@@ -21,27 +31,10 @@ driver = client.driver(id)
 # Documentation:
 # - https://www.selenium.dev/documentation/webdriver/bidirectional/chrome_devtools/cdp_endpoint/
 # 
-# NOT WORKING YET !!!!
-# 
-driver.execute_cdp('Page.addScriptToEvaluateOnNewDocument(alert("Hooray! I did it!"))')
+driver.execute_cdp("Page.addScriptToEvaluateOnNewDocument", source: js1+js2)
 
 # Get the URL to scrape
 url = 'https://www.facebook.com/?filter=groups&sk=h_chr'
 
 # Open the URL
 driver.get(url)
-
-=begin
-# Get source code of inject.js library using OpenURI library
-uri = URI.parse('https://raw.githubusercontent.com/leandrosardi/intercept/main/lib/intercept.js')
-js = Net::HTTP.get(uri)
-
-# Inject the library into the page
-driver.execute_script(js)
-
-# Get the source code of the scraper
-js = File.read('./facebook_group_posts.js')
-
-# Execute the scraper
-driver.execute_script(js)
-=end
